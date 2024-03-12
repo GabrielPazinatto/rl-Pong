6a403d53aef339536e6ffbd0b00ef8198974c873
Gabriel Ricci Pazinatto <gabrielrpazinatto@gmail.com>
2024-03-12 02:22:52 -0300

    Added main and drawing

---
+++ DrawGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Raylib_cs;
+using rl_pong;
+
+
+namespace Draw
+{
+    public class DrawGame
+    {
+
+        public void DrawPlayer(Player player)
+        {
+            Raylib.DrawRectangleRec(player.GetRectangle(), Color.White);
+        }
+
+        public void DrawBall(Ball ball)
+        {
+            Raylib.DrawCircleV(
+                ball.GetPos(),
+                ball.GetRadius(),
+                Color.Yellow
+                );
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Raylib_cs;
+
+namespace rl_pong
+{
+    public class Program
+    {
+        public static Player player = new();
+        public static Ball ball = new();
+
+        public static int screenWidth = 1200;
+        public static int screenHeight = 900;
+
+        public static void Main()
+        {
+            Draw.DrawGame draw = new();
+
+            Raylib.SetTargetFPS(Raylib.GetMonitorRefreshRate(0));
+            Raylib.InitWindow(screenWidth, screenHeight, "Pong");
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DarkGray);
+
+                draw.DrawBall(ball);
+                draw.DrawPlayer(player);
+
+                ball.Move();
+                player.Move();
+
+                Raylib.EndDrawing();
+            }
+        }
+    }
+}
